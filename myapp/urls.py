728c5bc6aeb87ac72c7c972dbd087546112c0b20
Takayuki Kawazoe <kawazoe@zoa-tech.com>
2020-04-25 22:50:20 +0900

    7

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MyFirstView
+from .views import MyFirstView, PostCreateView
 
 urlpatterns = [
-    path('', MyFirstView.as_view())  # URLとViewを組み合わせる！
+   path('', MyFirstView.as_view(), name='index'),  # URLとViewを組み合わせる！
+   path('create/', PostCreateView.as_view(), name='create')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render,get_object_or_404
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+
 from .models import Post
+from .forms import PostForm
+
 
 class MyFirstView(ListView):
     template_name = "myapp/index.html"
@@ -9,3 +15,21 @@ class MyFirstView(ListView):
         posts = Post.objects.all()
         context = {'posts': posts}
         return render(request, self.template_name, context)
+
+
+class PostCreateView(CreateView):
+   """
+   ビュー：登録画面
+   """
+   model = Post
+   form_class = PostForm
+   success_url = reverse_lazy('index')
+
+   def form_valid(self, form):
+       """
+       登録処理
+       """
+       post = form.save(commit=False)
+       post.save()
+
+       return HttpResponseRedirect(self.success_url)
